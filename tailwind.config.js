e6db2797a508f6d51aa139fd7534d49a041f04b9
hyeran0513 <hr_0513@naver.com>
2025-09-17 18:18:11 +0900

    feat: 폰트 및 og 이미지 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,105 @@
+import plugin from "tailwindcss/plugin";
+import tailwindcssAnimate from "tailwindcss-animate";
+ 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+const config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pretendard: ["var(--font-pretendard)"],
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    tailwindcssAnimate,
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".heading-1": {
+          fontFamily: "var(--font-pretendard)",
+          fontWeight: "800",
+          fontSize: "32px",
+          lineHeight: "150%",
+          letterSpacing: "0%",
+        },
+        ".heading-2": {
+          fontFamily: "var(--font-pretendard)",
+          fontWeight: "800",
+          fontSize: "28px",
+          lineHeight: "150%",
+          letterSpacing: "0%",
+        },
+        ".heading-3": {
+          fontFamily: "var(--font-pretendard)",
+          fontWeight: "800",
+          fontSize: "20px",
+          lineHeight: "150%",
+          letterSpacing: "0%",
+        },
+        ".heading-4": {
+          fontFamily: "var(--font-pretendard)",
+          fontWeight: "600",
+          fontSize: "18px",
+          lineHeight: "150%",
+          letterSpacing: "0%",
+        },
+        ".heading-5": {
+          fontFamily: "var(--font-pretendard)",
+          fontWeight: "600",
+          fontSize: "16px",
+          lineHeight: "150%",
+          letterSpacing: "0%",
+        },
+        ".heading-6": {
+          fontFamily: "var(--font-pretendard)",
+          fontWeight: "600",
+          fontSize: "14px",
+          lineHeight: "150%",
+          letterSpacing: "0%",
+        },
+        ".heading-7": {
+          fontFamily: "var(--font-pretendard)",
+          fontWeight: "600",
+          fontSize: "12px",
+          lineHeight: "150%",
+          letterSpacing: "0%",
+        },
+        ".body-1": {
+          fontFamily: "var(--font-pretendard)",
+          fontWeight: "400",
+          fontSize: "20px",
+          lineHeight: "150%",
+          letterSpacing: "0%",
+        },
+        ".body-2": {
+          fontFamily: "var(--font-pretendard)",
+          fontWeight: "400",
+          fontSize: "16px",
+          lineHeight: "150%",
+          letterSpacing: "0%",
+        },
+        ".body-3": {
+          fontFamily: "var(--font-pretendard)",
+          fontWeight: "400",
+          fontSize: "14px",
+          lineHeight: "150%",
+          letterSpacing: "0%",
+        },
+        ".body-4": {
+          fontFamily: "var(--font-pretendard)",
+          fontWeight: "400",
+          fontSize: "12px",
+          lineHeight: "150%",
+          letterSpacing: "0%",
+        },
+      });
+    }),
+  ],
+};
 
+export default config;
