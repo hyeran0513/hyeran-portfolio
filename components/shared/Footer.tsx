708bc7b82ae1e71d91b8d3c9f03be8326178f811
hyeran0513 <hr_0513@naver.com>
2025-09-17 22:17:42 +0900

    feat: 푸터 생성

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,26 @@
+import { GITHUB_URL, EMAIL } from '@/constants/service';
+
+const Footer = () => {
+  return (
+    <footer className="border-t border-border-primary h-[118px]">
+      <div className="max-w-[1200px] mx-auto px-[64px] h-full flex justify-between items-center">
+        {/* 저작권 */}
+        <p className="body-3">© 2025 김혜란. All rights reserved.</p>
+
+        <div className="flex items-center gap-[48px]">
+          {/* 깃허브 */}
+          <a href={GITHUB_URL} target="_blank" rel="noreferrer" className="body-3 cursor-pointer">
+            GitHub
+          </a>
+
+          {/* 이메일 */}
+          <a href={`mailto:${EMAIL}`} className="body-3 cursor-pointer">
+            Contact
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- constants/service.ts
+++ constants/service.ts
@@ -1,3 +1,5 @@
 export const SERVICE_URL = '';
 export const SERVICE_NAME = '김혜란 포트폴리오';
 export const SERVICE_DESCRIPTION = 'Next.js, tailwind css로 만든 김혜란 포트폴리오입니다.';
+export const EMAIL = 'hr_0513@naver.com';
+export const GITHUB_URL = 'https://github.com/hyeran0513';
